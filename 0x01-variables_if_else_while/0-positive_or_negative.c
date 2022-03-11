9fe997a2f6f76bf4f0cb34b76cc51175ced51968
Francis Bello <bellofrancis87@gmail.com>
2022-03-11 06:28:26 -0800

    Finally fixed the betty error for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		prontf("%d is negative\n", n);
 	}
 	else
 	{
